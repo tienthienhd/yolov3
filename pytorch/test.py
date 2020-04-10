71cee8b4d18d46cec0742363be257c7ef39b2eda
thiennt <tienthienhd@gmail.com>
2020-04-10 08:25:03 +0700

    darknet

--- pytorch/test.py
+++ pytorch/test.py
@@ -1,8 +1,10 @@
-
 from darknet import parse_cfg, create_modules
 from pprint import pprint
+import torch
+from darknet import get_test_input, Darknet
 
-blocks = parse_cfg("cfg/yolov3.cfg")
-# pprint(blocks)
-# print(create_modules(blocks))
-
+model = Darknet("cfg/yolov3.cfg")
+model.load_weights('cfg/yolov3.weights')
+inp = get_test_input()
+pred = model(inp, torch.cuda.is_available())
+print(pred.shape)
